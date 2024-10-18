172bf14df29861232176b65c02d11229e1a5d1da
Tahyuddin <tahyu4@gmail.com>
2024-10-18 19:21:57 +0530

    new try

---
+++ Simple/python.py
@@ -0,0 +1,14 @@
+class Test:
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a+b
+
+    def sub(self, a, b):
+        return a-b
+
+    def squre(self, a):
+        return a*a
+
